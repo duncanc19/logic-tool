ace48ace695eb37a5246621c2aca5b08e8867a49
D.N.Carter <duncancarter93@gmail.com>
2020-05-28 15:57:13 +0100

    basic parsing of two formulae into trees

--- script.js
+++ script.js
@@ -79,22 +79,79 @@ function selectTextBox(textBox) {
 /* PARSING FORMULAE INTO TREES */
 
 function setupProof() {
-  let originalTree = buildTree(formulaInput.value);
-  let finalTree = buildTree(transformedFormula.value);
+  let originalArray = buildArray(formulaInput.value);
+  let finalArray = buildArray(transformedFormula.value);
+  let originalTree = { value: originalArray, children: []};
+  let finalTree = { value: finalArray, children: []};
+  buildTree(originalTree);
+  buildTree(finalTree);
+
   mainBody.innerHTML = "<h1>" + formulaInput.value + " ≡ " + transformedFormula.value +
   "</h1><br><p>" + formulaInput.value + "</p>";
   console.log(originalTree);
   console.log(finalTree);
 }
 
-function buildTree(formula) {
-  let tree = [];
+
+function buildArray(formula) {
+  let array = [];
 
   for (i=0; i<formula.length; i++) {
     if (formula[i] == " ") { // do nothing
     } else {
-      tree.push(formula[i]);
+      array.push(formula[i]);
     }
   }
-  return tree;
+  return array;
+}
+
+function buildTree(node) {
+  if (node.value.length == 1) {
+    node.value = node.value[0];
+  } else if (findSymbol(node.value, '⇔')) {
+      setNodeAndChildren(node, '⇔');
+  } else if (findSymbol(node.value, '⇒')) {
+      setNodeAndChildren(node, '⇒');
+  } else if (findSymbol(node.value, '∨')) {
+      setNodeAndChildren(node, '∨');
+  } else if (findSymbol(node.value, '∧')) {
+      setNodeAndChildren(node, '∧');
+  } else if (findSymbol(node.value, '¬')) {
+    setNodeAndChildren(node, '¬');
+  }
+  console.log(node);
+  node.children.forEach(child => buildTree(child));
+}
+
+
+
+
+function findSymbol(nodeValue, symbol) {
+  for (i=0; i<nodeValue.length; i++) {
+    if (nodeValue[i] == symbol) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function setNodeAndChildren(node,symbol) {
+  let leftChild = { value: [], children: [] };
+  let rightChild = { value: [], children: [] };
+  // finds symbol, puts left of symbol in leftChild, right of symbol in rightChild
+  for (i=0; i<node.value.length; i++) {
+    if (node.value[i] == symbol) {
+      for (j=0; j<i; j++) {
+        leftChild.value.push(node.value[j]);
+      }
+      for (k=i+1; k<node.value.length; k++) {
+        rightChild.value.push(node.value[k]);
+      }
+      if (leftChild.value != []) {
+        node.children.push(leftChild);
+      }
+      node.children.push(rightChild);
+      node.value = symbol;
+    }
+  }
 }
