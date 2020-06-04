d1ffa96a35dae50ea8f579d4f4cff1076c78b7cb
D.N.Carter <duncancarter93@gmail.com>
2020-06-04 09:19:56 +0100

    fixed idempotence basic functionality

--- toolRules.js
+++ toolRules.js
@@ -1,21 +1,11 @@
 /* RULES */
 function idempotenceRule(original) {
-  let originalSection = buildTreeFromString(original);
-  if (originalSection.value === '∧' || originalSection.value === '∨') {
-    return true;
-  }
-  return false;
-}
-  buildTreeFromString(changed);
-  if
-  );
-} else if (findSymbol(node.value, ''
-}
-
-function idempotenceCheckChildren(rootNode) {
-  if (rootNode.children[0].value === rootNode.children[1].value) {
-    rootNode.children[0].forEach()
-    rootNode.children[0].children.forEach(child => )
+  let node = buildTreeFromString(original);
+  if (node.value === '∧' || node.value === '∨') {
+    if (node.children[0].value === node.children[1].value) {
+      node = node.children[0];
+      return node;
+    }
   }
 }
 
