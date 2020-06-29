581dcc4a9518f4f671a5bed118a54cf49f3b5347
D.N.Carter <duncancarter93@gmail.com>
2020-06-29 18:34:19 +0100

    Added modals for nicer error messages

--- script.js
+++ script.js
@@ -58,7 +58,27 @@ function selectTextBox(textBox) {
     currentTextBox = document.getElementById(textBox);
 }
 
+/* ALERT BOXES/MODALS */
+const modal = document.getElementById("myModal");
+const span = document.getElementsByClassName("close")[0];
+const modalText = document.getElementById("modalText");
 
+function showAlert(text) {
+  modalText.innerHTML = text;
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+} 
 
 /* SETTING UP PROBLEM SOLUTION FEATURES */
 
@@ -84,7 +104,7 @@ function setupProof() {
     originalTree = buildTreeFromString(formulaInput.value);
     finalTree = buildTreeFromString(transformedFormula.value);
   } catch {
-    alert('The formulae you have entered are not valid. Please check them and look out for problems such as unclosed brackets.');
+    showAlert('The formulae you have entered are not valid. Please check them and look out for problems such as unclosed brackets.');
     return;
   }
 
