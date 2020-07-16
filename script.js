6109b30d17082a73e18b5fb3ac3f7f1e5aae8a08
D.N.Carter <duncancarter93@gmail.com>
2020-07-16 09:48:03 +0100

    Moved showAlert functionality into local scope

--- script.js
+++ script.js
@@ -125,25 +125,27 @@ function selectTextBox(textBox) {
 
 /* ALERT BOXES/MODALS */
 const modal = document.getElementById("myModal");
-const span = document.getElementsByClassName("close")[0];
-const modalText = document.getElementById("modalText");
 
 function showAlert(text) {
+  const span = document.getElementsByClassName("close")[0];
+  const modalText = document.getElementById("modalText");
+
   modalText.innerHTML = text;
   modal.style.display = "block";
-}
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
     modal.style.display = "none";
   }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
 }
+
 
 /* SETTING UP PROBLEM SOLUTION FEATURES */
 const rulesInTool = `<div id="rulesInTool"><h5>Rules</h5>
